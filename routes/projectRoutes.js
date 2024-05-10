5c5ccabaf55e40e86ef6e0143298c539cbc6f9e6
Souvik Ghosh <129908887+Souvik-Ghosh-js@users.noreply.github.com>
2024-05-10 16:49:29 +0530

    get al projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { add_project } = require('../controllers/projectController');
+const { add_project , get_all_projects } = require('../controllers/projectController');
 const { restrictToAdmin } = require('../passportConfig');
 
 
 // Route to add a new project
 router.post('/add', restrictToAdmin ,add_project);
+router.get('/get', get_all_projects )
+
 
 module.exports = router;
